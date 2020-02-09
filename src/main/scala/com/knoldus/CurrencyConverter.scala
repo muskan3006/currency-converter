55aed02268db8fa585883c0dac595c1fe000171b
Muskan Gupta <muskan.gupta@knoldus.com>
2020-02-10 00:13:10 +0530

    Adding the feature by which we can convert an amount in one currency to another

---
+++ src/main/scala/com/knoldus/CurrencyConverter.scala
@@ -0,0 +1,56 @@
+package com.knoldus
+import math._
+/**
+ * In this class we do the task of converting the amount from one type of currency to another type
+ */
+class CurrencyConverter {
+  /**
+   * This map stores the conversion rates corresponding to their currency name for conversion in Indian Rupees
+   */
+  private val conversionRateFromInr: Map[String, Double] = Map("usd" -> 0.014, "euro" -> 0.013, "cad" -> 0.019,
+    "pkr" -> 2.16, "gbp" -> 0.011, "aud" -> 0.021, "baht" -> 0.44, "yen" -> 1.53, "idr" -> 191.31, "sgd" -> 0.019,
+    "inr" -> 1.00)
+  /**
+   * This map stores the conversion rates for conversion from indian rupees
+   */
+  private val conversionRateToInr: Map[String, Double] = Map("usd" -> 71.52, "euro" -> 78.28, "cad" -> 53.73,
+    "pkr" -> 0.46, "gbp" -> 92.20, "aud" -> 47.72, "baht" -> 2.28, "yen" -> 0.65, "idr" -> 0.0052, "sgd" -> 51.46)
+
+  /**
+   * A method that calls 2 functions to get the converted amount
+   *
+   * @param from   the currency from which the amount has to be converted
+   * @param to     the currency to which the amount has to be converted
+   * @param amount it denotes the amount to be converted
+   * @return
+   */
+  def converter(from: String, to: String, amount: Double): Double = {
+    if (to.toLowerCase != from.toLowerCase) {
+      val equivalentInr = convertToInr(from.toLowerCase, amount)
+      val convertedAmount = convertFromInr(to.toLowerCase, equivalentInr)
+      convertedAmount
+    }
+    else {
+      amount
+    }
+  }
+
+  /**
+   * This method converts the amount to Indian Rupees
+   *
+   * @param from   the currency from which the amount has to be converted
+   * @param amount it denotes the amount to be converted in Indian Rupees
+   * @return
+   */
+
+  def convertToInr(from: String, amount: Double): Double =  amount * conversionRateToInr(from)
+
+  /**
+   * This method converts the Indian Rupees to the required currency
+   *
+   * @param to     the currency to which the amount has to be converted
+   * @param amount it converts the Indian Rupees to the required currency
+   * @return
+   */
+  def convertFromInr(to: String, amount: Double): Double = amount * conversionRateFromInr(to)
+}
---
+++ src/test/scala/com/knoldus/CurrencyConverterSpec.scala
@@ -0,0 +1,38 @@
+package com.knoldus
+
+import org.scalatest._
+
+class CurrencyConverterSpec extends FlatSpec with BeforeAndAfterAll {
+  var currencyConverter: CurrencyConverter = new CurrencyConverter
+
+  override def beforeAll(): Unit = {
+    currencyConverter = new CurrencyConverter
+  }
+
+  override def afterAll(): Unit = {
+    if (currencyConverter  != null) {
+      currencyConverter  = null
+    }
+  }
+  "Converter method" should "return amount if from and to are same" in {
+    val actualResult = currencyConverter.converter("inr","INR",21)
+    val expectedResult = 21
+    assert(expectedResult == actualResult)
+  }
+
+  "Converter method" should "return equivalent amount if converted to Indian Rupees" in {
+    val actualResult = currencyConverter.converter("pkr","InR",21)
+    val expectedResult = 9.66
+    assert(expectedResult == actualResult)
+  }
+
+  "Converter method" should "return converted amount if converted to other currency" in {
+    val actualResult = currencyConverter.converter("yen","auD",21)
+    val expectedResult = 0.28665
+    assert(expectedResult == actualResult)
+  }
+
+
+
+
+}
